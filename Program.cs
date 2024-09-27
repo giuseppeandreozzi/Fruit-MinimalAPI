9b7e8b9a0cc548f42c887632370098f90e492634
giuseppeandreozzi <g.andreozzi98@gmail.com>
2024-09-27 10:48:20 +0200

    Some improvements and added parameter validation
    Parameter Validation achieved by using MinimalApis.Extensions package and DataAnnotations attribute

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System.Collections.Concurrent;
+using System.ComponentModel.DataAnnotations;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddProblemDetails();
@@ -10,25 +11,41 @@ else
 
 app.UseStatusCodePages();
 
-ConcurrentDictionary<string, Fruit> fruits = new ConcurrentDictionary<string, Fruit>();
-fruits.TryAdd("apple", new Fruit(1, "apple"));
+ConcurrentDictionary<int, Fruit> fruits = new ConcurrentDictionary<int, Fruit>();
+fruits.TryAdd(1, new Fruit(1, "apple"));
 int lastId = 1;
 
 app.MapGet("/fruit/list", () => fruits);
-app.MapGet("/fruit/{id}", (string id) => {
-    Fruit? fruit;
-    if (fruits.TryGetValue(id, out fruit))
+app.MapGet("/fruit", (int id) => {
+    if (fruits.TryGetValue(id, out Fruit? fruit))
         return TypedResults.Ok(fruit);
     else
         return Results.ValidationProblem(new Dictionary<string, string[]> { { "id", ["Fruit doesn't exist"] } });
     });
 
-app.MapPut("/fruit/add/{name}", (string name) => fruits.TryAdd(name, new Fruit(++lastId, name)) ? Results.Ok("Fruit added") : 
+app.MapPut("/fruit/add", (Fruit fruit) => fruits.TryAdd(++lastId, new Fruit(fruit.weigth, fruit.name)) ? Results.Ok("Fruit added") : 
                                                     Results.ValidationProblem(new Dictionary<string, string[]> { { "name", ["Fruit already exist"] } })
-);
+).WithParameterValidation();
 
-app.MapDelete("/fruit/delete/{name}", (string name) => fruits.TryRemove(name, out _) ? Results.Ok("Fruit deleted") :
-                                                    Results.ValidationProblem(new Dictionary<string, string[]> { { "name", ["Fruit doesn't exist"] } }));
+app.MapPut("/fruit/edit/{id}", (Fruit fruit, int id) => {
+    fruits.TryGetValue(id, out Fruit? currentFruit);
+    return fruits.TryUpdate(id, fruit, currentFruit) ? Results.Ok("Fruit edited") :
+                                                    Results.ValidationProblem(new Dictionary<string, string[]> { { "name", ["Fruit already exist"] } });
+}).WithParameterValidation();
+
+app.MapDelete("/fruit/delete", (int id) => fruits.TryRemove(id, out _) ? Results.Ok("Fruit deleted") :
+                                                    Results.ValidationProblem(new Dictionary<string, string[]> { { "name", ["Fruit doesn't exist"] } })).WithParameterValidation();
 app.Run();
 
-record Fruit(int id, string name);
+record Fruit{
+    [Required]
+    public int weigth { get; set; }
+    [Required, 
+     StringLength(100)]
+    public string name { get; set; }
+
+    public Fruit(int weigth, string name) {
+        this.weigth = weigth;
+        this.name = name;
+    }
+}
